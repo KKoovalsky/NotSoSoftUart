4d0ebbf0a417d449c0bc183ba75ae0e335e8de43
Kacper <kacper.s.kowalski@gmail.com>
2018-01-26 18:24:33 +0100

    Rename 'rcv' to 'rx'. Push byte to circ-buf common
    Renaming was made to have the equivalent name for 'tx'.
    
    Since now a common function is defined for pushing byte to a circular
    buffer. Pushing byte to the RX buffer uses this function.

--- nss_uart.c
+++ nss_uart.c
@@ -1,33 +1,31 @@
 #include "nss_uart.h"
 #include <stdio.h>
+#include <stdbool.h>
 #include <inttypes.h>
 
 #define set_bits(byte, num, pos)		byte |= ( ( 1 << num ) - 1 ) << pos
 
-#define UART_RCV_BUF_SIZE               256
+#define UART_RX_BUF_SIZE               256
 
 const int frame_len = 8;
 
-static volatile uint8_t uart_rcv_buf[UART_RCV_BUF_SIZE];
-static volatile unsigned int uart_rcv_buf_head, uart_rcv_buf_tail;
+static volatile uint8_t uart_rx_buf[UART_RX_BUF_SIZE];
+static volatile unsigned int uart_rx_buf_head, uart_rx_buf_tail;
 
 volatile unsigned int nssu_bytes_rcvd = 0;
 
 extern void init_not_so_soft_uart();
-extern void start_nssu_timer();
-extern void stop_nssu_timer();
-extern void reset_nssu_timer();
-extern int get_nssu_pin_state();
-extern int get_nssu_bytes_rcvd();
+extern void start_nssu_rx_timer();
+extern void stop_nssu_rx_timer();
+extern void reset_nssu_rx_timer();
+extern int get_nssu_rx_pin_state();
+extern int get_nssu_bits_rcvd();
 
-static inline void push_byte_to_circ_buf(uint8_t val, volatile uint8_t *buf, volatile unsigned int *buf_head, 
-		size_t buf_size)
-{
-	buf[*buf_head] = val;
-	*buf_head = (*buf_head + 1) % buf_size;
-}
+static void push_byte_to_circ_buf(uint8_t val, volatile uint8_t *buf, volatile unsigned int *head, size_t size);
+static void push_byte_to_rx_buf(uint8_t val);
 
-void handle_nssu_pin_change()
+
+void handle_nssu_rx_pin_change()
 {
     static int bit_cnt = -2;
     static uint8_t byte_rcvd = 0;
@@ -36,16 +34,16 @@ void handle_nssu_pin_change()
 	if(bit_cnt == -2)
     {
 		// Start the timer to count bits received
-        start_nssu_timer();
+        start_nssu_rx_timer();
         bit_cnt++;
         return;
     } 
     
-    int n = get_nssu_bytes_rcvd();
+    int n = get_nssu_bits_rcvd();
 	
 	// Make appropriate bit shift basing on actual state of the pin. 
 	// If pin is high then '0' bits have been received or '1' bits otherwise.
-	if(!get_nssu_pin_state())
+	if(!get_nssu_rx_pin_state())
 		set_bits(byte_rcvd, n, bit_cnt);
 	
     bit_cnt += n;
@@ -55,18 +53,31 @@ void handle_nssu_pin_change()
 	// This situation is fatal, because it could cause desynchronization of algorithm.
     if(bit_cnt >= frame_len)
     {
-		push_byte_to_circ_buf(byte_rcvd, uart_rcv_buf, &uart_rcv_buf_head, UART_RCV_BUF_SIZE);
+		push_byte_to_rx_buf(byte_rcvd);
         bit_cnt = -2;
         byte_rcvd = 0;
-        stop_nssu_timer();
+        stop_nssu_rx_timer();
     } else
 	{
 		// If not all bits have been received then reset the timer.
-        reset_nssu_timer();
+        reset_nssu_rx_timer();
 	}
 }
 
-void handle_nssu_tim_overflow()
+void handle_nssu_rx_tim_overflow()
 {
 	nssu_bytes_rcvd++;
+static void push_byte_to_circ_buf(uint8_t val, volatile uint8_t *buf, volatile unsigned int *head, size_t size)
+{
+	unsigned int h = *head;
+	buf[h] = val;
+	h = ( h + 1 ) % size;
+	*head = h;
+}
+
+static void push_byte_to_rx_buf(uint8_t val)
+{
+	push_byte_to_circ_buf(val, uart_rx_buf, &uart_rx_buf_head, UART_RX_BUF_SIZE);
+}
+
 }
--- nss_uart.h
+++ nss_uart.h
@@ -1,9 +1,14 @@
 #ifndef NSS_UART_H
 #define NSS_UART_H
 
+#include <inttypes.h>
 extern volatile unsigned int nssu_bytes_rcvd;
 
-void handle_nssu_pin_change();
-void handle_nssu_tim_overflow();
+// Should be called within on edge detected ISR for RX pin
+void handle_nssu_rx_pin_change();
+
+// Should be called within ISR which handles bit counting for the receiver
+void handle_nssu_rx_tim_overflow();
+
 
 #endif /* NSS_UART_H */
